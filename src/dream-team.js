3149c969a44aa5f8d210d01e7ef80f92e1daa4a4
asnouski <osnovvv@gmail.com>
2021-03-15 11:47:10 +0300

    feat: code in carbon and dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+	let team = [];
+	if (typeof (members) !== 'object' || !members || members.length == 0) return false;
+	for (let i = 0; i < members.length; i++) {
+		if (typeof (members[i]) == 'string') {
+			let name = members[i].toUpperCase().trim();
+			team.push(name[0]);
+		}
+	}
+	return team.sort().join('');
 };
