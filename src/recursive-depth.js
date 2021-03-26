7901b9cd44a7cba9951313da93ceb659ee492fb0
asnouski <osnovvv@gmail.com>
2021-03-26 17:31:16 +0300

    feat: add code in hanoi

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+	calculateDepth(arr) {
+		let total = 1;
+		arr.forEach(elem => {
+			if (Array.isArray(elem)) total = Math.max(total, 1 + this.calculateDepth(elem))
+		});
+		return total;
+	}
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-	if (typeof (arr) != 'object') throw new CustomError('Not implemented');
 	let newArr = [];
+	if (typeof (arr) != 'object') throw new CustomError('Not implemented');
+	if (typeof (arr[0]) == 'object') return newArr
 	let r, next;
-	for (let i = 0; i < arr.length; i++) {
-		newArr[i] = arr[i]
-		if (arr[i] == '--double-next') newArr[i] = arr[i + 1];
-		if (arr[i] == '--double-prev') newArr[i] = arr[i - 1];
-		if (arr[i] == '--discard-next') {
-			next = 1
-			r = i;
-		}
-		if (arr[i] == '--discard-prev') {
-			next = -1
-			r = i;
-		}
+	if (typeof (arr[0]) == 'string' || typeof (arr[arr.length - 1]) == 'string') {
+		newArr = arr.filter(elem => { return typeof (elem) != 'string' })
+		return newArr
 	}
-	if (next === 1) newArr.splice(r, 2);
-	if (next === -1) newArr.splice(r - 1, 2);
+	if (typeof (arr[0]) != 'string' || typeof (arr[arr.length - 1]) != 'string') {
+		console.log(1)
+		for (let i = 0; i < arr.length; i++) {
+			newArr[i] = arr[i]
+			if (arr[i] == '--double-next') newArr[i] = arr[i + 1];
+			if (arr[i] == '--double-prev') newArr[i] = arr[i - 1];
+			if (arr[i] == '--discard-next') {
+				next = 1
+				r = i;
+			}
+			if (arr[i] == '--discard-prev') {
+				next = -1
+				r = i;
+			}
+		}
+		if (next === 1) newArr.splice(r, 2);
+		if (next === -1) newArr.splice(r - 1, 2);
+	}
 	return newArr
 };
